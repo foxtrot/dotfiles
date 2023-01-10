3f8b1057cec1766485129d1be7a583744d9c6e1a
Marc <marc@malloc.me>
2023-01-10 14:05:31 +1100

    nvim: correctly use silentops bind config

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,31 +1,31 @@
 -- Options default for silent mappings
-local silentopts = { noremap = true, silent = true }
+local silentops = { noremap = true, silent = true }
 
 -- Configure leader
 vim.g.mapleader = ' '
 
 -- Create splits
-vim.api.nvim_set_keymap('n', '<leader>s', ':sp', opts)
-vim.api.nvim_set_keymap('n', '<leader>v', ':vsp', opts)
+vim.api.nvim_set_keymap('n', '<leader>s', ':sp', silentops)
+vim.api.nvim_set_keymap('n', '<leader>v', ':vsp', silentops)
 
 -- Move between splits
-vim.api.nvim_set_keymap('n', '<C-S-Left>', ':wincmd h', opts)
-vim.api.nvim_set_keymap('n', '<C-S-Down>', ':wincmd j', opts)
-vim.api.nvim_set_keymap('n', '<C-S-Up>', ':wincmd k', opts)
-vim.api.nvim_set_keymap('n', '<C-S-Right>', ':wincmd l', opts)
+vim.api.nvim_set_keymap('n', '<C-S-Left>', ':wincmd h', silentops)
+vim.api.nvim_set_keymap('n', '<C-S-Down>', ':wincmd j', silentops)
+vim.api.nvim_set_keymap('n', '<C-S-Up>', ':wincmd k', silentops)
+vim.api.nvim_set_keymap('n', '<C-S-Right>', ':wincmd l', silentops)
 
 -- Move between buffers
-vim.api.nvim_set_keymap('n', '<C-Left>', ':bnext', opts)
-vim.api.nvim_set_keymap('n', '<C-Right>', ':bprev', opts)
-vim.api.nvim_set_keymap('n', '<leader>c', ':close', opts)
-vim.api.nvim_set_keymap('n', '<leader>d', ':bdelete', opts)
+vim.api.nvim_set_keymap('n', '<C-Left>', ':bnext', silentops)
+vim.api.nvim_set_keymap('n', '<C-Right>', ':bprev', silentops)
+vim.api.nvim_set_keymap('n', '<leader>c', ':close', silentops)
+vim.api.nvim_set_keymap('n', '<leader>d', ':bdelete', silentops)
 
 -- Save file
-vim.api.nvim_set_keymap('n', '<leader>w', ':w', opts)
-vim.api.nvim_set_keymap('n', '<leader>W', ':wall', opts)
+vim.api.nvim_set_keymap('n', '<leader>w', ':w', silentops)
+vim.api.nvim_set_keymap('n', '<leader>W', ':wall', silentops)
 
 -- Clear search highlight
-vim.api.nvim_set_keymap('n', '<leader>z', ':noh', opts)
+vim.api.nvim_set_keymap('n', '<leader>z', ':noh', silentops)
 
 -- Search for visually selected text
 vim.api.nvim_set_keymap('v', '//', 'y/\\V<C-R>=escape(@",\'/\')')
